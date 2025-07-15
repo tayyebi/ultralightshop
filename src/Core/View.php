a663993b3ae3b17522c18687c50e0eb3ad40922f
tayyebi <smile@tyyi.net>
2025-07-16 02:53:01 +0330

    wrongly bound args to constructor.

--- src/Core/View.php
+++ src/Core/View.php
@@ -5,7 +5,7 @@ class View
 {
     protected $is_lazyload;
 
-    public function __construct(callable $content)
+    public function __construct()
     {
         $this->is_lazyload = isset($_SERVER['HTTP_X_LAZYLOAD_NAV']) && $_SERVER['HTTP_X_LAZYLOAD_NAV'] === '1';
     }
